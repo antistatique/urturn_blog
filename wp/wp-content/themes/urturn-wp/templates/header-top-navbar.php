ab3f8985ee311b5e4006e7ea2c1bcfa32596e353
Marc Friederich <marc@antistatique.net>
2013-04-12 00:25:37 +0200

    remove collapse navigation

--- wp/wp-content/themes/urturn-wp/templates/header-top-navbar.php
+++ wp/wp-content/themes/urturn-wp/templates/header-top-navbar.php
@@ -1,20 +1,18 @@
-<header class="banner navbar navbar-static-top" role="banner">
+<header class="banner navbar navbar-fixed-top" role="banner">
   <div class="navbar-inner">
     <div class="container">
-      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
+      <a class="brand urturn" href="<?php echo home_url(); ?>/">
+        <strong><?php bloginfo('name'); ?></strong>
       </a>
-      <a class="brand" href="<?php echo home_url(); ?>/">
-        <?php bloginfo('name'); ?>
-      </a>
-      <nav class="nav-main nav-collapse collapse" role="navigation">
+      <nav class="nav-main" role="navigation">
         <?php
           if (has_nav_menu('primary_navigation')) :
             wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav'));
           endif;
         ?>
+        <div class="navbar-search pull-right">
+          <?php get_template_part('templates/searchform'); ?>
+        </div>
       </nav>
     </div>
   </div>
