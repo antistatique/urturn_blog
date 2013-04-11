6d0ad4927d436a7e9f051643837c1ab054838fd2
Marc Friederich <marc@antistatique.net>
2013-04-12 00:25:51 +0200

    add footer content

--- wp/wp-content/themes/urturn-wp/templates/footer.php
+++ wp/wp-content/themes/urturn-wp/templates/footer.php
@@ -1,8 +1,41 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
-    <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
   </div>
+</footer>
+<!-- Footer
+================================================== -->
+<footer class="footer">
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="span7">
+                    <h2>Get the latest insights</h2>
+                    <p>Drop us your email so we can update you on the latest fan engagement insights on a pleasantly infrequent basis.</p>
+                    
+                    <form action="http://webdoc.createsend.com/t/r/s/jjurhut/" method="post" id="newsletter-form-bottom">
+                        <div class="input-append">
+                            <input type="text" placeholder="Email" name="cm-jjurhut-jjurhut" id="jjurhut-jjurhut" class="span2" />
+                            <input type="submit" class="btn" value="Subscribe" />
+                        </div>
+                    </form>
+
+                </div> 
+
+                <div class="span3 offset2">
+                    <div class="footer-menu">
+                      <ul>
+                        <li><a id="blog-link" href="http://blog.urturn.com"><i class="blog"></i> Blog</a></li>
+                        <li><a id="contact-link" data-toggle="modal" href="#contact-form-modal"><i class="contact"></i> Contact us</a></li>
+                        <!-- Contact Form -->
+                        <li><a id="dev-link" href="http://developers.urturn.com"><i class="dev"></i> Developers</a></li>
+
+                      </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </footer>
 
 <?php wp_footer(); ?>
